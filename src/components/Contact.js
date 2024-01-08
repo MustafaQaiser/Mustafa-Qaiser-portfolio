37fdb3ba5bdd07f4e58b6178e1f99817d6f502bc
MustafaQaiser <mustafaqaiser78@gmail.com>
2024-01-08 16:09:22 +0500

    forth commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
       <h2>Contact</h2>
       <p>Phone: 03247727786</p>
       <p>Email: mustafaqaiser78@gmail.com</p>
-      <a href="linkedin.com/in/mustafa-qaiser-1bbb4a247" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/in/mustafa-qaiser-1bbb4a247" target="_blank" rel="noopener noreferrer">
         LinkedIn
       </a>
       <a href="https://github.com/MustafaQaiser" target="_blank" rel="noopener noreferrer">
